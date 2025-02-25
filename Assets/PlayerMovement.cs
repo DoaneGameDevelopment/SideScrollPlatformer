f456f51348c4856065830c7b7d1987d51a73a97b
BRRRARRGH <thure.w.johnson@gmail.com>
2025-02-24 23:56:07 -0600

    Left Right Movement + Jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody2D rigidBody;
+    public float speed;
+    public float verticalSpeed;
+    private float Move;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.linearVelocityY += 5;
+        }
+        Move = Input.GetAxis("Horizontal");
+        rigidBody.linearVelocity = new Vector2(Move * speed, rigidBody.linearVelocityY);
         
     }
 }
