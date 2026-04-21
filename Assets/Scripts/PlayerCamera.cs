aac7d5af36b02b53b001f37b813181e18d391134
Johang <johang.hernandez@doane.edu>
2026-04-21 12:30:57 -0500

    Finished making a lower limit | #65 (#67)
    Finished making a lower limit

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,12 +6,17 @@ public class PlayerCamera : MonoBehaviour
     public float speed = 0.12f;
 
     private Vector3 smoothPosition;
+    private float limited_lower_y;
+
+    public float lower_y_limit = -3f;
 
 
     void FixedUpdate() {
+
+        limited_lower_y = Mathf.Max(lower_y_limit, player.position.y);
         // fixedUpdate basically ties to the physics engine timing, rather than each frame, therefore, frame studders don't lag the camera.
-        smoothPosition = Vector3.Lerp(this.transform.position, new Vector3(player.position.x, player.position.y, this.transform.position.z), speed);
+        smoothPosition = Vector3.Lerp(this.transform.position, new Vector3(player.position.x, limited_lower_y, this.transform.position.z), speed);
         this.transform.position = smoothPosition;
-        Vector3 mouse = Input.mousePosition;
+        // Vector3 mouse = Input.mousePosition;
     }
 }
